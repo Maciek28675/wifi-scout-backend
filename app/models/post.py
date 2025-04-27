efccc02bff32db47e7a6b296d44b3e4ea425168f
Maciej Rybarczyk <maciej.rybarczyk@onet.pl>
2025-04-27 15:50:22 +0200

    Refeactor project structure

---
+++ app/models/post.py
@@ -0,0 +1,8 @@
+from imports import *
+
+class Post(Base):
+    __tablename__ = 'posts'
+    post_id = Column(Integer, primary_key=True)
+    user_id = Column(Integer, ForeignKey('users.user_id'))
+    content = Column(String(500)) # liczba znakow w poscie do ustalenia
+    location = Column(String(50))
---
+++ app/models/user.py
@@ -0,0 +1,8 @@
+from imports import *
+
+class User(Base):
+    __tablename__ = 'users'
+    user_id = Column(Integer, primary_key=True)
+    email = Column(String(50), unique=True, nullable=False)
+    password = Column(String(50), nullable=False)
+    signUpDate = Column(Date, default=datetime.now().date())
